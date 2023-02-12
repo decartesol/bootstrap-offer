a277e2f490789202b4c4b699de29edf7d08dfd88
decartesol <123273990+decartesol@users.noreply.github.com>
2023-02-12 11:37:17 +0000

    update website icon

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Decartes Web 3</title>
-        {/*<link rel="icon" href="/favicon.ico" />*/}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Intro />
