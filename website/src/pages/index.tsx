4031b2936fad31e91a69e1f4370124357262dd0b
decartesol <123273990+decartesol@users.noreply.github.com>
2023-02-12 11:42:23 +0000

    add basePath to icon url

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { Calculator } from '@/content/calculator'
 import { Header } from '@/content/header'
 import { Intro } from '@/content/intro'
 import Head from 'next/head'
+import {basePath} from '../../next.config';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Decartes Web 3</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
       <Header />
       <Intro />
